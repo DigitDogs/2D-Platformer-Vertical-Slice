a832d4830ab4d64a67404aacba5de5fbbb473d7c
Joost Wolters <digitdogs@gmail.com>
2019-12-03 18:32:33 +0100

    Deleted unnecessary files and made an map.

---
+++ 2D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private int speed, jumpcount;
+
+    [SerializeField]
+    private float horizontalMove = 0f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        // Searches and attatches the rigidbody2d to rb
+        if (this.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            rb = this.gameObject.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if (horizontalMove == -1f)
+        {
+
+        }
+        else if (horizontalMove == 1)
+        {
+
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+        }
+    }
+}
